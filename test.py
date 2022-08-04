f42f94cce42f6688506b12987b61ae0e7f5a0f7b
Giritharram <giritharram005@gmail.com>
2022-08-05 03:26:52 +0530

    Parsed all the Headers

--- test.py
+++ test.py
@@ -2,15 +2,22 @@ from dataclasses import replace
 from email.parser import BytesParser, Parser
 from email.policy import default
 import re
-import os
-import sys
+
 
 
 sh=[]
 rh=[]
 pu=[]
 tl=[]
-l = ['Message-ID: <','Subject: ','From: ','Reply-To: ','To: ','pradeep']
+
+ls = ['Message-ID: ','Subject: ','From: ','Reply-To: ','To: ','Date: ']
+
+lo = ['Accept-Language: ','Approved: ','ARC-Authentication-Results: ','ARC-Message-Signature: ','ARC-Seal: ','Archive: ','Archived-At: ','Authentication-Results: ','Auto-Submitted: ','Bcc: ','Body: ','Cancel-Key: ','Cancel-Lock: ','Cc: ','Comments: ','Alternate-Recipient: ','Autoforwarded: ','Autosubmitted: ','Content-Alternative: ','Content-Description: ','Content-Disposition: ','Content-Duration: ','Content-features: ','Content-ID: ','Content-Identifier: ','Content-Language: ','Content-Location: ','Content-MD5: ','Content-Return: ','Content-Transfer-Encoding: ',
+'Content-Translation-Type: ','Content-Type: ','Control: ','Conversion: ','Conversion-With-Loss: ','DL-Expansion-History: ','Deferred-Delivery: ','Delivery-Date: ','Discarded-X400-IPMS-Extensions: ','Discarded-X400-MTS-Extensions: ','Disclose-Recipients: ','Disposition-Notification-Options: ','Disposition-Notification-To: ','Distribution: ','DKIM-Signature: ','Downgraded-Final-Recipient: ','Downgraded-In-Reply-To: ','Downgraded-Message-Id: ','Downgraded-Original-Recipient: ','Downgraded-References: ','Encoding: ','Encrypted: ','Expires: ','Expiry-Date: ','Followup-To: ','Generate-Delivery-Report: ',
+'Importance: ','In-Reply-To: ','Incomplete-Copy: ','Injection-Date: ','Injection-Info: ','Keywords: ','Language: ','Latest-Delivery-Time: ','Lines: ','List-Archive: ','List-Help: ','List-ID: ','List-Owner: ','List-Owner: ','List-Subscribe: ','List-Unsubscribe: ','List-Unsubscribe-Post: ','Message-Context: ','Message-ID: ','Message-Type: ','MIME-Version: ','MMHS-Exempted-Address: ','MMHS-Extended-Authorisation-Info: ','MMHS-Subject-Indicator-Codes: ','MMHS-Handling-Instructions: ','MMHS-Message-Instructions: ','MMHS-Codress-Message-Indicator: ','MMHS-Originator-Reference: ','MMHS-Primary-Precedence: ','MMHS-Copy-Precedence: ',
+'MMHS-Message-Type: ','MMHS-Other-Recipients-Indicator-To: ','MMHS-Other-Recipients-Indicator-CC: ','MMHS-Acp127-Message-Identifier: ','MMHS-Originator-PLAD: ','MT-Priority: ','Newsgroups: ','Obsoletes: ','Organization: ','Original-Encoded-Information-Types: ','Original-From: ','Original-Message-ID: ','Original-Recipient: ','Original-Sender: ','Originator-Return-Address: ','Original-Subject: ','Path: ','PICS-Label: ','Posting-Version: ','Prevent-NonDelivery-Report: ','Priority: ','Received-SPF: ','References: ','Relay-Version: ','Reply-By: ','Require-Recipient-Valid-Since: ','Resent-Bcc: ','Resent-Cc: ','Resent-Date: ','Resent-From: ',
+'Resent-Message-ID: ','Resent-Reply-To: ','Resent-Sender: ','Resent-To: ','Return-Path: ','Sender: ','Sensitivity: ','Solicitation: ','Summary: ','Supersedes: ','TLS-Report-Domain: ','TLS-Required: ','TLS-Report-Submitter: ','User-Agent: ','VBR-Info: ','VBR-Info: ','X400-Content-Identifier: ','X400-Content-Return: ','X400-Content-Type: ','X400-MTS-Identifier: ','X400-Originator: ','X400-Received: ','X400-Recipients: ','X400-Trace: ','Xref: ']
+
 
 with open('sample.txt', 'rb') as fp:
 
@@ -86,6 +93,17 @@ with open('sample.txt', 'rb') as fp:
 			print('{} {}'.format(b,d[0]))
 		except:
 			None
+	
+	def call_summary():
+		for i in ls:
+			if i in str(headers):
+				summary(i)
+		print("\n")
+
+	def call_otherheaders():
+		for i in lo:
+			if i in str(headers):
+				summary(i)
 
 	def X_headers():
 		with open('sample.txt', 'rb') as fp:
@@ -100,50 +118,46 @@ with open('sample.txt', 'rb') as fp:
 	
 
 	if __name__ == "__main__":
-		# sys.stdout = open('a.txt','wt')
 
-		# print("-------------------------------------------------------------")
-		# print("			Sender_host		")
-		# print("-------------------------------------------------------------")
-		# sender_host(a)
+
+		print("-------------------------------------------------------------")
+		print("			Sender_host		")
+		print("-------------------------------------------------------------")
+		sender_host(a)
 		
 
-		# print("-------------------------------------------------------------")
-		# print("			Received_host		")
-		# print("-------------------------------------------------------------")
-		# received_host(a)
+		print("-------------------------------------------------------------")
+		print("			Received_host		")
+		print("-------------------------------------------------------------")
+		received_host(a)
 
-		# print("\n")
-		# print("-------------------------------------------------------------")
-		# print("			 No of Hops			")
-		# print("-------------------------------------------------------------")
-		# print("         		   ",len(rh))
-		
-		# print("\n")
-		# print("-------------------------------------------------------------")
-		# print("			Protocol Used		")
-		# print("-------------------------------------------------------------")
-		# protocol_used(a)
-		
-		# print("-------------------------------------------------------------")
-		# print("			Time Stamp		")
-		# print("-------------------------------------------------------------")
-		# time_stamp(a)
-
-		# print("-------------------------------------------------------------")
-		# print("			Summary		")
-		# print("-------------------------------------------------------------")
-		for i in l:
-			if i in str(headers):
-				summary(i)
-			else:
-				print("Header Not Found!")
 		print("\n")
+		print("-------------------------------------------------------------")
+		print("			 No of Hops			")
+		print("-------------------------------------------------------------")
+		print("         		   ",len(rh))
 		
-		# print("-------------------------------------------------------------")
-		# print("			X-Headers		")
-		# print("-------------------------------------------------------------")
-		# X_headers()
+		print("\n")
+		print("-------------------------------------------------------------")
+		print("			Protocol Used		")
+		print("-------------------------------------------------------------")
+		protocol_used(a)
+		
+		print("-------------------------------------------------------------")
+		print("			Time Stamp		")
+		print("-------------------------------------------------------------")
+		time_stamp(a)
+
+		print("-------------------------------------------------------------")
+		print("			Summary		")
+		print("-------------------------------------------------------------")
+		call_summary()
+		
+		print("-------------------------------------------------------------")
+		print("			Other-Headers		")
+		print("-------------------------------------------------------------")
+		call_otherheaders()
+		X_headers()
 		
 
 
--- test2.py
+++ test2.py
@@ -1,70 +1,43 @@
-import os
-from test import *
-
-os.system("python3 test.py")
-
-a1 = []
-b1 = []
-res= []
-
-with open('sample.txt','r') as fp:
-    for i in fp:
-        a = str(i).replace('\n','')
-        t = list(a.split(" "))
-        f = list(filter(None,t))
-        a1.append(f)
-    # for i in a1:
-    #     print(i)
+from dataclasses import replace
+from email.parser import BytesParser, Parser
+from email.policy import default
+import re
 
 
-with open('a.txt','r') as fa:
-    for i in fa:
-        a = str(i).replace('\n','')
-        t = list(a.split(" "))
-        f = list(filter(None,t))
-        b1.append(f)
+ls = ['Message-ID: <','Subject: ','From: ','Reply-To: ','To: ']
+
+lo = ['Accept-Language: ','Approved: ','ARC-Authentication-Results: ','ARC-Message-Signature: ','ARC-Seal: ','Archive: ','Archived-At: ','Authentication-Results: ','Auto-Submitted: ','Bcc: ','Body: ','Cancel-Key: ','Cancel-Lock: ','Cc: ','Comments: ','Alternate-Recipient: ','Autoforwarded: ','Autosubmitted: ','Content-Alternative: ','Content-Description: ','Content-Disposition: ','Content-Duration: ','Content-features: ','Content-ID: ','Content-Identifier: ','Content-Language: ','Content-Location: ','Content-MD5: ','Content-Return: ','Content-Transfer-Encoding: ',
+'Content-Translation-Type: ','Content-Type: ','Control: ','Conversion: ','Conversion-With-Loss: ','DL-Expansion-History: ','Deferred-Delivery: ','Delivery-Date: ','Discarded-X400-IPMS-Extensions: ','Discarded-X400-MTS-Extensions: ','Disclose-Recipients: ','Disposition-Notification-Options: ','Disposition-Notification-To: ','Distribution: ','DKIM-Signature: ','Downgraded-Final-Recipient: ','Downgraded-In-Reply-To: ','Downgraded-Message-Id: ','Downgraded-Original-Recipient: ','Downgraded-References: ','Encoding: ','Encrypted: ','Expires: ','Expiry-Date: ','Followup-To: ','Generate-Delivery-Report: ',
+'Importance: ','In-Reply-To: ','Incomplete-Copy: ','Injection-Date: ','Injection-Info: ','Keywords: ','Language: ','Latest-Delivery-Time: ','Lines: ','List-Archive: ','List-Help: ','List-ID: ','List-Owner: ','List-Owner: ','List-Subscribe: ','List-Unsubscribe: ','List-Unsubscribe-Post: ','Message-Context: ','Message-ID: ','Message-Type: ','MIME-Version: ','MMHS-Exempted-Address: ','MMHS-Extended-Authorisation-Info: ','MMHS-Subject-Indicator-Codes: ','MMHS-Handling-Instructions: ','MMHS-Message-Instructions: ','MMHS-Codress-Message-Indicator: ','MMHS-Originator-Reference: ','MMHS-Primary-Precedence: ','MMHS-Copy-Precedence: ',
+'MMHS-Message-Type: ','MMHS-Other-Recipients-Indicator-To: ','MMHS-Other-Recipients-Indicator-CC: ','MMHS-Acp127-Message-Identifier: ','MMHS-Originator-PLAD: ','MT-Priority: ','Newsgroups: ','Obsoletes: ','Organization: ','Original-Encoded-Information-Types: ','Original-From: ','Original-Message-ID: ','Original-Recipient: ','Original-Sender: ','Originator-Return-Address: ','Original-Subject: ','Path: ','PICS-Label: ','Posting-Version: ','Prevent-NonDelivery-Report: ','Priority: ','Received-SPF: ','References: ','Relay-Version: ','Reply-By: ','Require-Recipient-Valid-Since: ','Resent-Bcc: ','Resent-Cc: ','Resent-Date: ','Resent-From: ',
+'Resent-Message-ID: ','Resent-Reply-To: ','Resent-Sender: ','Resent-To: ','Return-Path: ','Sender: ','Sensitivity: ','Solicitation: ','Summary: ','Supersedes: ','TLS-Report-Domain: ','TLS-Required: ','TLS-Report-Submitter: ','User-Agent: ','VBR-Info: ','VBR-Info: ','X400-Content-Identifier: ','X400-Content-Return: ','X400-Content-Type: ','X400-MTS-Identifier: ','X400-Originator: ','X400-Received: ','X400-Recipients: ','X400-Trace: ','Xref: ']
 
 
-def flatten(t):
-    fl=[]
-    for i in t:
-        if type(i) is list:
-            for item in i:
-                fl.append(item)
-        else:
-            fl.append(i)
-    return fl
+with open('sample.txt','rb') as fp:
+    headers = BytesParser(policy=default).parse(fp)
+    a = str(headers).split('Received: ')
+
+    def summary(b):
+        try:
+            tmp = str(headers).split(b)
+            
+        except:
+            None
+        try:
+            a = tmp[1]
+        except:
+            None
+        d = list(a.split("\n"))
+        
+        try:
+            print('{} {}'.format(b,d[0]))
+
+        except:
+            None
 
 
-def rm_common(a,b):
-    for i in a[:]:
-        if i in b:
-            a.remove(i)
-    return a
-
-# a = rm_common(a1,b1)
-# print(a)
-
-# a = flatten(a1)
-# b = flatten(b1)
-c = rm_common(a1,b1)
-
-d = rm_common(c,l)
-for i in d:
-    res.append(i)
-    
-print(res)
-
-# def re_headers(c):
-#     a = ""
-#     try:
-#         for i in c:
-#             li = i.remove(str(l))
-#             res.append(li)            
-#     except:
-#         None
-#     return res
-
-
-# print(re_headers(c))
-    
+    for i in lo:
+        if i in str(headers):
+            summary(i)
+        
+            
