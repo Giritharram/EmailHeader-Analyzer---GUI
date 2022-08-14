a80dc7e38bf88edd78d4c70160f49ab5c9d383fc
Giritharram <giritharram005@gmail.com>
2022-08-14 23:18:30 +0530

    Parsed Domain names

--- test2.py
+++ test2.py
@@ -1,145 +1,170 @@
-# from dataclasses import replace
-# from email.parser import BytesParser, Parser
-# from email.policy import default
+# from pdb import pm
+# import requests
 # import re
+# import socket
+
+# ips = []
+# lts = []
+# fli = []
+# fnl = []
+# op  = []
+# pmin = 21
+# pmax = 500
+
+# def flatten(l):
+#     fl=[]
+#     for i in l:
+#         if type(i) is list:
+#             for item in i:
+#                 fl.append(item)
+#         else:
+#             fl.append(i)
+#     return fl
 
 
-# ls = ['Message-ID: <','Subject: ','From: ','Reply-To: ','To: ']
+# with open('sample.txt', 'r') as file:
+#     fi = file.readlines()
+#     re_ip = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
 
-# lo = ['Received-SPF: ','Accept-Language: ','Approved: ','ARC-Authentication-Results: ','ARC-Message-Signature: ','ARC-Seal: ','Archive: ','Archived-At: ','Authentication-Results: ','Auto-Submitted: ','Bcc: ','Body: ','Cancel-Key: ','Cancel-Lock: ','Cc: ','Comments: ','Alternate-Recipient: ','Autoforwarded: ','Autosubmitted: ','Content-Alternative: ','Content-Description: ','Content-Disposition: ','Content-Duration: ','Content-features: ','Content-ID: ','Content-Identifier: ','Content-Language: ','Content-Location: ','Content-MD5: ','Content-Return: ','Content-Transfer-Encoding: ',
-# 'Content-Translation-Type: ','Content-Type: ','Control: ','Conversion: ','Conversion-With-Loss: ','DL-Expansion-History: ','Deferred-Delivery: ','Delivery-Date: ','Discarded-X400-IPMS-Extensions: ','Discarded-X400-MTS-Extensions: ','Disclose-Recipients: ','Disposition-Notification-Options: ','Disposition-Notification-To: ','Distribution: ','DKIM-Signature: ','Downgraded-Final-Recipient: ','Downgraded-In-Reply-To: ','Downgraded-Message-Id: ','Downgraded-Original-Recipient: ','Downgraded-References: ','Encoding: ','Encrypted: ','Expires: ','Expiry-Date: ','Followup-To: ','Generate-Delivery-Report: ',
-# 'Importance: ','In-Reply-To: ','Incomplete-Copy: ','Injection-Date: ','Injection-Info: ','Keywords: ','Language: ','Latest-Delivery-Time: ','Lines: ','List-Archive: ','List-Help: ','List-ID: ','List-Owner: ','List-Owner: ','List-Subscribe: ','List-Unsubscribe: ','List-Unsubscribe-Post: ','Message-Context: ','Message-ID: ','Message-Type: ','MIME-Version: ','MMHS-Exempted-Address: ','MMHS-Extended-Authorisation-Info: ','MMHS-Subject-Indicator-Codes: ','MMHS-Handling-Instructions: ','MMHS-Message-Instructions: ','MMHS-Codress-Message-Indicator: ','MMHS-Originator-Reference: ','MMHS-Primary-Precedence: ','MMHS-Copy-Precedence: ',
-# 'MMHS-Message-Type: ','MMHS-Other-Recipients-Indicator-To: ','MMHS-Other-Recipients-Indicator-CC: ','MMHS-Acp127-Message-Identifier: ','MMHS-Originator-PLAD: ','MT-Priority: ','Newsgroups: ','Obsoletes: ','Organization: ','Original-Encoded-Information-Types: ','Original-From: ','Original-Message-ID: ','Original-Recipient: ','Original-Sender: ','Originator-Return-Address: ','Original-Subject: ','Path: ','PICS-Label: ','Posting-Version: ','Prevent-NonDelivery-Report: ','Priority: ','References: ','Relay-Version: ','Reply-By: ','Require-Recipient-Valid-Since: ','Resent-Bcc: ','Resent-Cc: ','Resent-Date: ','Resent-From: ',
-# 'Resent-Message-ID: ','Resent-Reply-To: ','Resent-Sender: ','Resent-To: ','Return-Path: ','Sender: ','Sensitivity: ','Solicitation: ','Summary: ','Supersedes: ','TLS-Report-Domain: ','TLS-Required: ','TLS-Report-Submitter: ','User-Agent: ','VBR-Info: ','VBR-Info: ','X400-Content-Identifier: ','X400-Content-Return: ','X400-Content-Type: ','X400-MTS-Identifier: ','X400-Originator: ','X400-Received: ','X400-Recipients: ','X400-Trace: ','Xref: ']
+# for line in fi:
+#     ip = re.findall(re_ip,line)
+#     ips.append(ip)
+
+# res = list(filter(None,ips))
+
+# for i in res:
+#     if len(i) > 1:
+#         lts.append(i[0])
+#         lts.append(i[1])
+#     else:
+#         lts.append(i)
+
+# for i in lts:
+#     if type(i) is str:
+#         fli.append(list(i.split(" ")))
+#     else:
+#         fli.append(i)
+
+# b = flatten(fli)
 
 
+# res = set(b)
+
+# fnl = list(set(res))
 
 
-# def summary(b):
-#     with open('sample.txt','rb') as fp:
-    
-#         for i in fp:
-#             z = str(i).strip("b'")
-#             try:
-#                 tmp =str(z).replace('\\n','').split(b)
-#             except:
-#                 None
-#             try:
-#                 a = tmp[1]
-            
-#             except:
-#                 None
-#         try:
-#             print('{} {}'.format(b,a))
-#         except:
-#             None
-    
-    # def summary(b):
-    #     try:
-    #         tmp = str(fp).split(b) 
-    #     except:
-    #         None
-    #     try:
-    #         a = tmp[1]
-    #     except:
-    #         None
-    #     d = list(a.split("\n"))
-        
-    #     try:
-    #         print('{} {}'.format(b,d[0]))
+# for k in fnl:
+#     url = ("https://www.virustotal.com/api/v3/ip_addresses/%s" % k)
+#     headers = {
+#         "Accept": "application/json",
+#         "x-apikey": "0016029bda9f2888c76cd394c44f3ab11ee24ddc092c81523060f2294fe29e0a"
+#     }
+#     r = requests.get(url, headers=headers).json()
+#     try:
+#         dict_web = r['data']['attributes']['last_analysis_results']
 
-    #     except:
-    #         None
+#         tot_engine_c=0
+#         tot_detect_c=0
+#         result_eng = []
+#         eng_name = []
+#         count_harmless = 0
+#         for i in dict_web:
+#             tot_engine_c = 1 + tot_engine_c
+#             if dict_web[i]['category'] == "malicious" or dict_web[i]['category'] == "suspicious":
+#                 result_eng.append(dict_web[i]["result"])
+#                 eng_name.append(dict_web[i]["engine_name"])
+#                 tot_detect_c = 1 + tot_detect_c
+#         res = []
+#         for i in result_eng:
+#             if i not in res:
+#                 res.append(i)
 
-# with open('sample.txt','rb') as fp:
-#     headers = BytesParser(policy=default).parse(fp)
-#     for i in lo:
-#         if i in str(headers):
-#             summary(i)
+#         result_eng = res
+#         if tot_detect_c > 0:
+#             print("The IP %s was rated for " % i + str(result_eng)[1:-1] + " on " + str(tot_detect_c) + " engines out of " + str(tot_engine_c) + " engines. \n")
+#             print("The reported engines are: \n")
+#             for i in eng_name:
+#                 print(i)
+#         else:
+#             print("The IP %s   has been marked harmless" %k)
+#     except:
+#         None
 
-    
-import requests
+# ip = '5.2.72.226'
+
+# for port in range(pmin,pmax+1):
+#     try:
+#         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#             s.settimeout(0.5)
+#             s.connect((ip,port))
+#             op.append(port)
+#     except:
+#         None
+
+# for port in op:
+#     print(f"Port {port} is open on {ip}.")
 import re
-ips = []
-lts = []
-fli = []
-fnl = []
+import urllib
+import urllib.request
+dnwe = []
+dnwht = []
+dnwhts = []
+# Get TLD database
+resp = urllib.request.urlopen('http://data.iana.org/TLD/tlds-alpha-by-domain.txt')
 
-def flatten(l):
-    fl=[]
-    for i in l:
-        if type(i) is list:
-            for item in i:
-                fl.append(item)
-        else:
-            fl.append(i)
-    return fl
+# Create a reverse sorted list of TLD ('.com' must be before '.co')
+tld = sorted([tld.strip().lower().decode('utf-8')
+                  for tld in resp.readlines()[1:]], reverse=True)
 
-
-with open('sample.txt', 'r') as file:
-    fi = file.readlines()
-    re_ip = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
-
-for line in fi:
-    ip = re.findall(re_ip,line)
-    ips.append(ip)
-
-res = list(filter(None,ips))
-
-for i in res:
-    if len(i) > 1:
-        lts.append(i[0])
-        lts.append(i[1])
-    else:
-        lts.append(i)
-
-for i in lts:
-    if type(i) is str:
-        fli.append(list(i.split(" ")))
-    else:
-        fli.append(i)
-
-b = flatten(fli)
-
-res = set(b)
-
-fnl = list(set(res))
+# Compile the regex pattern
+FQDN = re.compile(fr"([^\s]*\.(?:{'|'.join(tld)}))")
 
 
+# Find all fqdn
+with open('sample.txt') as fp:
+    fqdn_list = []
+    for line in fp.readlines():
+        line = line.strip().lower()
 
-for k in fnl:
-    url = ("https://www.virustotal.com/api/v3/ip_addresses/%s" % k)
-    headers = {
-        "Accept": "application/json",
-        "x-apikey": "0016029bda9f2888c76cd394c44f3ab11ee24ddc092c81523060f2294fe29e0a"
-    }
-    r = requests.get(url, headers=headers).json()
-    try:
-        dict_web = r['data']['attributes']['last_analysis_results']
+        # Remove comments and blank lines
+        if (len(line) == 0) or line.startswith('#'):
+            continue
+        # Extract FQDN
+        fqdn = FQDN.findall(line)
+        if fqdn:
+            fqdn_list.append(fqdn[0])
 
-        tot_engine_c=0
-        tot_detect_c=0
-        result_eng = []
-        eng_name = []
-        count_harmless = 0
-        for i in dict_web:
-            tot_engine_c = 1 + tot_engine_c
-            if dict_web[i]['category'] == "malicious" or dict_web[i]['category'] == "suspicious":
-                result_eng.append(dict_web[i]["result"])
-                eng_name.append(dict_web[i]["engine_name"])
-                tot_detect_c = 1 + tot_detect_c
-        res = []
-        for i in result_eng:
-            if i not in res:
-                res.append(i)
 
-        result_eng = res
-        if tot_detect_c > 0:
-            print("The IP %s was rated for " % i + str(result_eng)[1:-1] + " on " + str(tot_detect_c) + " engines out of " + str(tot_engine_c) + " engines. \n")
-            print("The reported engines are: \n")
-            for i in eng_name:
-                print(i)
-        else:
-            print("The IP %s   has been marked harmless" %k)
-    except:
-        None
+# for i in fqdn_list:
+    # if '=' in i and '"' not in i: 
+    #     tmp1 = i.split('=')
+        # print(tmp)
+    # if 'http' in i:
+    #     try:
+    #         tmp2 = i.split('http:')
+    #         try:
+    #             dnwht.append('http:'+tmp2[1])
+    #         except:
+    #             None
+    #     except:
+    #         None
+    # if 'https' in i:
+    #     try:
+    #         tmp2 = i.split('https')
+    #         dnwhts.append('https'+tmp2[1])
+    #     except:
+    #         None
+    # if len(i)>=10:
+    #     print(i,"No")
+    # print(i)
+
+# print(dnwht)
+# print('------')
+# print(dnwhts)
+# print(len(fqdn_list))
+
+with open("sample.txt") as file:
+        for line in file:
+            urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', line)
+            print(urls)
+# a = str(fqdn_list).split()
