aefbcac4aef87e11fb9053928a10d331cbae8594
Giritharram <giritharram005@gmail.com>
2022-08-20 01:03:39 +0530

    GUI Parsed all Headers

--- fl1.py
+++ fl1.py
@@ -10,7 +10,10 @@ def result():
    reh = received_host()
    protu = protocol_used()
    timst = time_stamp()
-   return render_template('result.html', no_of_hop=hop,sender_result = seh,received_result = reh,proto_used=protu,timestamp_result=timst)
+   sumry = call_summary()
+   othehe = combine_Xh_and_Oh()
+   return render_template('result.html', no_of_hop=hop,sender_result=seh,received_result=reh,proto_used=protu,timestamp_result=timst,
+   summary_result=sumry,otherheaders_result=othehe)
 
 if __name__ == '__main__':
    app.run(debug = True)
--- test.py
+++ test.py
@@ -13,6 +13,15 @@ ls = ['Message-ID: ','Subject: ','From: ','Reply-To: ','To: ','Date: ']
 lo = ['Accept-Language: ','Approved: ','ARC-Authentication-Results: ','ARC-Message-Signature: ','ARC-Seal: ','Archive: ','Archived-At: ','Authentication-Results: ','Auto-Submitted: ','Bcc: ','Body: ','Cancel-Key: ','Cancel-Lock: ','Cc: ','Comments: ','Alternate-Recipient: ','Autoforwarded: ','Autosubmitted: ','Content-Alternative: ','Content-Description: ','Content-Disposition: ','Content-Duration: ','Content-features: ','Content-ID: ','Content-Identifier: ','Content-Language: ','Content-Location: ','Content-MD5: ','Content-Return: ','Content-Transfer-Encoding: ','Content-Translation-Type: ','Content-Type: ','Control: ','Conversion: ','Conversion-With-Loss: ','DL-Expansion-History: ','Deferred-Delivery: ','Delivery-Date: ','Discarded-X400-IPMS-Extensions: ','Discarded-X400-MTS-Extensions: ','Disclose-Recipients: ','Disposition-Notification-Options: ','Disposition-Notification-To: ','Distribution: ','DKIM-Signature: ','Downgraded-Final-Recipient: ','Downgraded-In-Reply-To: ','Downgraded-Message-Id: ','Downgraded-Original-Recipient: ','Downgraded-References: ','Encoding: ','Encrypted: ','Expires: ','Expiry-Date: ','Followup-To: ','Generate-Delivery-Report: ','Importance: ','In-Reply-To: ','Incomplete-Copy: ','Injection-Date: ','Injection-Info: ','Keywords: ','Language: ','Latest-Delivery-Time: ','Lines: ','List-Archive: ','List-Help: ','List-ID: ','List-Owner: ','List-Owner: ','List-Subscribe: ','List-Unsubscribe: ','List-Unsubscribe-Post: ','Message-Context: ','Message-ID: ','Message-Type: ','MIME-Version: ','MMHS-Exempted-Address: ','MMHS-Extended-Authorisation-Info: ','MMHS-Subject-Indicator-Codes: ','MMHS-Handling-Instructions: ','MMHS-Message-Instructions: ','MMHS-Codress-Message-Indicator: ','MMHS-Originator-Reference: ','MMHS-Primary-Precedence: ','MMHS-Copy-Precedence: ',
 'MMHS-Message-Type: ','MMHS-Other-Recipients-Indicator-To: ','MMHS-Other-Recipients-Indicator-CC: ','MMHS-Acp127-Message-Identifier: ','MMHS-Originator-PLAD: ','MT-Priority: ','Newsgroups: ','Obsoletes: ','Organization: ','Original-Encoded-Information-Types: ','Original-From: ','Original-Message-ID: ','Original-Recipient: ','Original-Sender: ','Originator-Return-Address: ','Original-Subject: ','Path: ','PICS-Label: ','Posting-Version: ','Prevent-NonDelivery-Report: ','Priority: ','Received-SPF: ','References: ','Relay-Version: ','Reply-By: ','Require-Recipient-Valid-Since: ','Resent-Bcc: ','Resent-Cc: ','Resent-Date: ','Resent-From: ','Resent-Message-ID: ','Resent-Reply-To: ','Resent-Sender: ','Resent-To: ','Return-Path: ','Sender: ','Sensitivity: ','Solicitation: ','Summary: ','Supersedes: ','TLS-Report-Domain: ','TLS-Required: ','TLS-Report-Submitter: ','User-Agent: ','VBR-Info: ','VBR-Info: ','X400-Content-Identifier: ','X400-Content-Return: ','X400-Content-Type: ','X400-MTS-Identifier: ','X400-Originator: ','X400-Received: ','X400-Recipients: ','X400-Trace: ','Xref: ']
 
+def flatten(l):
+    fl=[]
+    for i in l:
+        if type(i) is list:
+            for item in i:
+                fl.append(item)
+        else:
+            fl.append(i)
+    return fl
 
 with open('sample.txt', 'rb') as fp:
 
@@ -98,10 +107,11 @@ with open('sample.txt', 'rb') as fp:
 			None
 		d = list(a.split("\n"))
 		try:
-			print('{} {}'.format(b,d[0]))
+			r = d[0]
 		except:
 			None
-	
+		return r
+
 	def otherheaders(b):
 		with open('sample.txt','rb') as fp:
 			for i in fp:
@@ -114,31 +124,58 @@ with open('sample.txt', 'rb') as fp:
 					a = tmp[1]
 				except:
 					None
-			try:
-				print('{} {}'.format(b,a))
-			except:
-				None
+			return a
 
 	def call_summary():
+		call_sum = []
 		for i in ls:
 			if i in str(headers):
-				summary(i)
-		print("\n")
+				call_sum.append(i+summary(i))
+		return call_sum
+
 
 	def call_otherheaders():
+		tmp = []
 		for i in lo:
 			if i in str(headers):
-				otherheaders(i)
+				tmp.append(i+otherheaders(i))
+		r = flatten(tmp)		
+		return r
 
 	def X_headers():
 		with open('sample.txt', 'rb') as fp:
+			tmp = []
 			for i in fp:
 				a = str(i).strip("b'")
 				b = a.split('X-')
 				try:
-					print("X-"+b[1].replace('\\n','').replace('\\r',''))
+					tmp.append("X-"+b[1].replace('\\n','').replace('\\r',''))
 				except:
 					None
+			return tmp
+
+	def combine_Xh_and_Oh():
+		t1 = call_otherheaders()
+		t2 = X_headers()
+		t3 = t1+t2
+		f = flatten(t3)
+		return f
+
+		# hdr = []
+		# hdr_val = []
+		# for i in f:
+		# 	e = i.split(':')
+		# 	try:
+		# 		if len(e[0])>4:
+		# 			hdr.append(e[0])
+		# 	except:
+		# 		None
+		# 	try:
+		# 		hdr_val.append(e[1])
+		# 	except:
+		# 		None
+		# print(len(hdr))
+		# print(len(hdr_val))
 
 
 	if __name__ == "__main__":
@@ -177,13 +214,12 @@ with open('sample.txt', 'rb') as fp:
 		print("-------------------------------------------------------------")
 		print("			Summary		")
 		print("-------------------------------------------------------------")
-		call_summary()
+		print(call_summary())
 		
 		print("-------------------------------------------------------------")
 		print("			Other-Headers		")
 		print("-------------------------------------------------------------")
-		call_otherheaders()
-		X_headers()
+		print(combine_Xh_and_Oh())
 		
 
 
